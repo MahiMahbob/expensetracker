30e7e12c4573a6a3d9760b8ae9435cdfb1183614
MahiMahbob <mahbob.alam1219@gmail.com>
2020-12-29 14:26:24 +0600

    some change

--- src/App.js
+++ src/App.js
@@ -5,16 +5,17 @@ import FormContainer from "./components/FormContainer";
 import Header from "./components/Header";
 import IncomeContainer from "./components/IncomeContainer";
 import ListContainer from "./components/ListContainer";
+import { useContextValue } from "./context/ContextProvider";
 
 function App() {
-
+  const { transictions} = useContextValue();
   return (
     <>
       <h2>Expense Tracker</h2>
       <div className='container'>
         <Header />
         <IncomeContainer />
-        <FilterContainer />
+        {transictions.length && <FilterContainer />}
         <ListContainer />
         <h3>Add new transaction</h3>
         <FormContainer />
